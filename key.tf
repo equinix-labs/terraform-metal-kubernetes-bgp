23bee1761a50ef4554ce08fc8523b5ab9dbaaf77
Enkel Prifti <43306174+enkelprifti98@users.noreply.github.com>
2020-02-26 16:47:31 -0500

    Update key.tf

--- key.tf
+++ key.tf
@@ -5,7 +5,7 @@ resource "tls_private_key" "default" {
 
 resource "local_file" "private_key_pem" {
 
-  depends_on = tls_private_key.default
+  depends_on = [tls_private_key.default[
 
   content    = tls_private_key.default.private_key_pem
   filename   = default.pem
@@ -13,7 +13,7 @@ resource "local_file" "private_key_pem" {
 
 resource "local_file" "public_key_pem" {
 
-  depends_on = tls_private_key.default
+  depends_on = [tls_private_key.default]
 
   content    = tls_private_key.default.public_key_pem
   filename   = default-pub.pem
@@ -21,14 +21,14 @@ resource "local_file" "public_key_pem" {
 
 resource "local_file" "public_key_openssh" {
 
-  depends_on = tls_private_key.default
+  depends_on = [tls_private_key.default]
 
   content    = tls_private_key.default.public_key_openssh
   filename   = default-openssh.pub
 }
 
 resource "null_resource" "chmod" {
-  depends_on = local_file.private_key_pem
+  depends_on = [local_file.private_key_pem]
 
   triggers = {
     local_file_private_key_pem = local_file.private_key_pem
