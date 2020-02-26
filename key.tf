7f4786dae4cac8eb4141f22fbf685c34e7ae72e3
Enkel Prifti <43306174+enkelprifti98@users.noreply.github.com>
2020-02-26 16:35:22 -0500

    Create key.tf
    Add tls-key access method to make it easier for new users.

---
+++ key.tf
@@ -0,0 +1,40 @@
+resource "tls_private_key" "default" {
+  algorithm   = "ECDSA"
+  ecdsa_curve = "P384"
+}
+
+resource "local_file" "private_key_pem" {
+
+  depends_on = tls_private_key.default
+
+  content    = tls_private_key.default.private_key_pem
+  filename   = default.pem
+}
+
+resource "local_file" "public_key_pem" {
+
+  depends_on = tls_private_key.default
+
+  content    = tls_private_key.default.public_key_pem
+  filename   = default-pub.pem
+}
+
+resource "local_file" "public_key_openssh" {
+
+  depends_on = tls_private_key.default
+
+  content    = tls_private_key.default.public_key_openssh
+  filename   = default-openssh.pub
+}
+
+resource "null_resource" "chmod" {
+  depends_on = local_file.private_key_pem
+
+  triggers = {
+    local_file_private_key_pem = local_file.private_key_pem
+  }
+
+  provisioner "local-exec" {
+    command = "chmod 600 default.pem"
+  }
+}
