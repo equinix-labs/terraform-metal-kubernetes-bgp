5d7e09084ed1f4ba3049108aeb5b1cf994f1439c
Enkel Prifti <43306174+enkelprifti98@users.noreply.github.com>
2020-02-26 17:08:54 -0500

    Update key.tf

--- key.tf
+++ key.tf
@@ -8,7 +8,7 @@ resource "local_file" "private_key_pem" {
   depends_on = [tls_private_key.default]
 
   content    = tls_private_key.default.private_key_pem
-  filename   = default.pem
+  filename   = "default.pem"
 }
 
 resource "local_file" "public_key_pem" {
@@ -16,7 +16,7 @@ resource "local_file" "public_key_pem" {
   depends_on = [tls_private_key.default]
 
   content    = tls_private_key.default.public_key_pem
-  filename   = default-pub.pem
+  filename   = "default-pub.pem"
 }
 
 resource "local_file" "public_key_openssh" {
@@ -24,7 +24,7 @@ resource "local_file" "public_key_openssh" {
   depends_on = [tls_private_key.default]
 
   content    = tls_private_key.default.public_key_openssh
-  filename   = default-openssh.pub
+  filename   = "default-openssh.pub"
 }
 
 resource "null_resource" "chmod" {
