878d8b792c8a38e03f3d7f60f1a11f67d802f7b0
Enkel Prifti <43306174+enkelprifti98@users.noreply.github.com>
2020-02-26 17:32:46 -0500

    Update key.tf

--- key.tf
+++ key.tf
@@ -8,7 +8,7 @@ resource "local_file" "private_key_pem" {
   depends_on = [tls_private_key.default]
 
   content    = tls_private_key.default.private_key_pem
-  filename   = "default.pem"
+  filename   = "cluster-private-key.pem"
 }
 
 resource "local_file" "public_key_pem" {
@@ -16,7 +16,7 @@ resource "local_file" "public_key_pem" {
   depends_on = [tls_private_key.default]
 
   content    = tls_private_key.default.public_key_pem
-  filename   = "default-pub.pem"
+  filename   = "cluster-public-key.pem"
 }
 
 resource "local_file" "public_key_openssh" {
@@ -24,7 +24,7 @@ resource "local_file" "public_key_openssh" {
   depends_on = [tls_private_key.default]
 
   content    = tls_private_key.default.public_key_openssh
-  filename   = "default-openssh.pub"
+  filename   = "cluster-openssh.pub"
 }
 
 resource "null_resource" "chmod" {
