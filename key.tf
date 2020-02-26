1f3515bea5056c173064ff653e7bddaabbb6e7ce
Enkel Prifti <43306174+enkelprifti98@users.noreply.github.com>
2020-02-26 16:56:29 -0500

    Update key.tf

--- key.tf
+++ key.tf
@@ -5,7 +5,7 @@ resource "tls_private_key" "default" {
 
 resource "local_file" "private_key_pem" {
 
-  depends_on = [tls_private_key.default[
+  depends_on = [tls_private_key.default]
 
   content    = tls_private_key.default.private_key_pem
   filename   = default.pem
