a3177efe25e315dfa5ca60a90a3a64277761deac
Enkel Prifti <43306174+enkelprifti98@users.noreply.github.com>
2020-03-31 11:26:49 -0400

    Update metallb.tf

--- metallb.tf
+++ metallb.tf
@@ -53,7 +53,7 @@ resource "null_resource" "setup_metallb" {
     inline = [
       "kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.9.3/manifests/namespace.yaml",
       "kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.9.3/manifests/metallb.yaml",
-      "kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"",
+      "kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey=\"$(openssl rand -base64 128)\"",
       "kubectl apply -f /tmp/metallb-config.yaml",
     ]
   }
