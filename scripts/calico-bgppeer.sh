3fa92b7ca629f7c9401dd0c933470e7afb18325b
Enkel Prifti <43306174+enkelprifti98@users.noreply.github.com>
2020-07-13 19:39:33 -0400

    Update calico-bgppeer.sh

--- scripts/calico-bgppeer.sh
+++ scripts/calico-bgppeer.sh
@@ -2,16 +2,27 @@
 
 set -x
 
-HOSTNAME=$1
-PEER_IP=$2
+API_KEY=$1
+INSTANCE_UUID=$2
+HOSTNAME=$3
+PEER_IPS_AMOUNT=($(curl -X GET -H "X-Auth-Token: ${API_KEY}" "https://api.packet.net/devices/${INSTANCE_UUID}/bgp/neighbors" | jq ".bgp_neighbors[].peer_ips | length"))
+
+i="0"
+
+while [ $i -lt $PEER_IPS_AMOUNT ]
+do
 
 cat << EOF | DATASTORE_TYPE=kubernetes KUBECONFIG=~/.kube/config calicoctl create -f -
 apiVersion: projectcalico.org/v3
 kind: BGPPeer
 metadata:
-  name: $HOSTNAME
+  name: $HOSTNAME-peer-$i
 spec:
-  peerIP: $PEER_IP
-  node: $HOSTNAME
+  peerIP: $(curl -X GET -H "X-Auth-Token: ${API_KEY}" "https://api.packet.net/devices/${INSTANCE_UUID}/bgp/neighbors" | jq -r ".bgp_neighbors[].peer_ips[$i]")
+  node: $HOSTNAME-peer-$i
   asNumber: 65530
 EOF
+
+i=$[$i+1]
+
+done
